7156cc83595f8e434c9c9e261dd1dd21ac8475f3
Humber <ahfernandez-es@udabol.edu.bo>
2022-01-18 08:52:56 -0400

    Move the reusable functions to the function's class

--- includes/stripe-classes/class-stripe-checkout.php
+++ includes/stripe-classes/class-stripe-checkout.php
@@ -44,13 +44,13 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
         }
 
         add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
-        add_action('wp_enqueue_scripts', array($this, 'payment_scripts'));
+        add_action('wp_enqueue_scripts',                                       array($this, 'payment_scripts'));
 
-        add_action('wc_ajax_sp_stripe_checkout_order', array($this, 'sp_stripe_checkout_order_callback'));
-        add_action('wc_ajax_sp_stripe_cancel_order', array($this, 'sp_stripe_cancel_order'));
+        add_action('wc_ajax_sp_stripe_checkout_order',                         array($this, 'sp_stripe_checkout_order_callback'));
+        add_action('wc_ajax_sp_stripe_cancel_order',                           array($this, 'sp_stripe_cancel_order'));
 
-        add_action('woocommerce_available_payment_gateways', array($this, 'sp_disable_gateway_for_order_pay'));
-        add_action('set_logged_in_cookie', array($this, 'sp_set_cookie_on_current_request'));
+        add_action('woocommerce_available_payment_gateways',                   array($this, 'sp_disable_gateway_for_order_pay'));
+        add_action('set_logged_in_cookie',                                     array($this, 'sp_set_cookie_on_current_request'));
 
         // Set stripe API key.
         \Stripe\Stripe::setApiKey($this->secret_key);
@@ -107,13 +107,13 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
         if ((is_checkout() || is_product() || is_cart() || is_add_payment_method_page())  && !is_order_received_page()) {
             wp_register_script('stripe_v3_js', 'https://js.stripe.com/v3/');
 
-            wp_enqueue_script('sp_checkout_script', SP_PLUGIN_URL . 'assets/js/sp-checkout.js', array('stripe_v3_js', 'jquery'), 1.0, true);
+            wp_enqueue_script('sp_checkout_script', SP_PLUGIN_URL . 'assets/js/sp-checkout-js.js', array('stripe_v3_js', 'jquery'), 1.0, true);
 
-            $public_key = $this->public_key;
-            $secret_key = $this->secret_key;
+            $publishable_key = $this->publishable_key;
+            $secret_key      = $this->secret_key;
 
             $sp_checkout_params = array(
-                'key'                                           => isset($public_key) ? $public_key : '',
+                'key'                                           => isset($publishable_key) ? $publishable_key : '',
                 'wp_ajaxurl'                                    => admin_url("admin-ajax.php"),
                 'wc_ajaxurl'                                    => WC_AJAX::get_endpoint('%%change_end%%'),
             );
@@ -129,7 +129,7 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
         $_COOKIE[LOGGED_IN_COOKIE] = $cookie;
     }
 
-    /**
+    /** 
      * Disable stripe checkout gateway for order-pay page 
      */
     function sp_disable_gateway_for_order_pay($available_gateways)
@@ -147,19 +147,16 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
     public function process_payment($order_id)
     {
         $order = wc_get_order($order_id);
-        $currency =  $order->get_currency();
-
-        $total = $this->get_stripe_amount($order->get_total(), get_woocommerce_currency());
+        $email = (WC()->version < '2.7.0') ? $order->billing_email : $order->get_billing_email();
+        $total = sp_functions()->get_stripe_amount($order->get_total(), get_woocommerce_currency());
 
         $session_data = array(
             'payment_method_types' => ['card'],
             'locale'               => 'auto',
             'success_url'          => home_url() . '/?wc-ajax=sp_stripe_checkout_order' . '&sessionid={CHECKOUT_SESSION_ID}' . '&order_id=' . $order_id . '&_wpnonce=' . wp_create_nonce('sp_checkout_nonce'),
             'cancel_url'           => home_url() . '/?wc-ajax=sp_stripe_cancel_order' . '&_wpnonce=' . wp_create_nonce('sp_checkout_nonce') . '&order_id=' . $order_id,
-
         );
 
-        $email = (WC()->version < '2.7.0') ? $order->billing_email : $order->get_billing_email();
 
         if (!empty($email)) {
             $session_data['customer_email'] = $email;
@@ -178,7 +175,7 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
                 $campaign_author = $product->post->post_author;
                 $stripe_receiver_account = get_user_meta($campaign_author, 'stripe_user_id', true);
 
-                if (!$this->minimum_fund_reached($product)) {
+                if (!sp_functions()->minimum_fund_reached($product)) {
                     $payment_mode = 'setup';
                 }
             }
@@ -214,6 +211,8 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
         }
 
         $session = \Stripe\Checkout\Session::create($session_data);
+        add_post_meta($order_id, '_stripe_checkout_session_id', $session->id);
+
 
         return  array(
             'result'   => 'success',
@@ -223,44 +222,7 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
 
 
     /**
-     * Checks if minimum fund raise of the campaign has been reached
-     * 
-     * @return bool 
-     */
-    private function minimum_fund_reached(\WC_Product $product)
-    {
-        // Check minimum funding in order to setup a payment or capture inmediatly
-        $product_id = $product->get_id();
-        $raised_percent = wpcf_function()->get_raised_percent($product_id);
-        $minimum_percent = get_post_meta($product_id, 'freeit-minimum-funding-required', true);
-
-        return $raised_percent > $minimum_percent;
-    }
-
-    function get_payment_session_checkout_url($session_id, $order)
-    {
-
-        return sprintf(
-            '#response=%s',
-            base64_encode(
-                wp_json_encode(
-                    array(
-                        'session_id' => $session_id,
-                        'order_id'      => (WC()->version < '2.7.0') ? $order->id : $order->get_id(),
-                        'time'          => rand(
-                            0,
-                            999999
-                        ),
-                    )
-                )
-            )
-        );
-    }
-
-    /**
      * creates order after checkout session is completed.
-     * 
-     * 
      */
     public function sp_stripe_checkout_order_callback()
     {
@@ -276,7 +238,6 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
         $order_time = date('Y-m-d H:i:s', time() + get_option('gmt_offset') * 3600);
 
         $session = \Stripe\Checkout\Session::retrieve($session_id);
-
         // Check payment mode, If it's "setup"
         if ($session->mode == "setup") {
             //Retrieve setup intent ID
@@ -290,12 +251,12 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
                 'source_type'    => 'card',
                 'status'         => 'Waiting minimum fund',
                 'paid'           => 'Paid',
-                'captured'       => 'Uncaptured'
+                'captured'       => 'Uncaptured',
+                'payment_method' => $setup_intent->payment_method,
             ];
         } else {
+
             $payment_id = $session->payment_intent;
-
-
             $payment_intent = \Stripe\PaymentIntent::retrieve($payment_id);
             $charge_details = $payment_intent->charges['data'];
 
@@ -303,10 +264,10 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
                 $charge_response = $charge;
             }
 
-            $data = $this->make_charge_params($charge_response, $order_id);
-        }
+            $data = sp_functions()->make_charge_params($charge_response, $order_id);
 
-        add_post_meta($order_id, '_sp_payment_id', $payment_id);
+            add_post_meta($order_id, '_sp_payment_id', $payment_id);
+        }
 
         if ($data['paid'] == 'Paid') {
 
@@ -316,12 +277,39 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
                 $order->update_status('on-hold');
             }
 
+            /**
+             * Create stripe customer
+             */
+
+            $user_id = $order->get_user_id();
+            $stripe_customer_id = get_user_meta($user_id, 'stripe_customer_id', true);
+            $payment_method = $data['payment_method'];
+
+            if (!$stripe_customer_id) {
+                $new_customer = \Stripe\Customer::create([
+                    'email'          => $order->get_billing_email(),
+                    'payment_method' =>  $payment_method,
+                ]);
+
+                $stripe_customer_id = $new_customer->id;
+
+                // Add metadata to user
+                update_user_meta($user_id, 'stripe_customer_id', $stripe_customer_id);
+            }
+
+            // Add metadata to order
+
+            add_post_meta($order_id, '_stripe_customer_id', $stripe_customer_id);
+            add_post_meta($order_id, '_stripe_customer_payment_method', $payment_method);
+
+            /**-------------------- */
+
             $order->set_transaction_id($data['transaction_id']);
 
             $order->add_order_note(__('Payment Status : ', 'payment-gateway-stripe-and-woocommerce-integration') . ucfirst($data['status']) . ' [ ' . $order_time . ' ] . ' . __('Source : ', 'payment-gateway-stripe-and-woocommerce-integration') . $data['source_type'] . '. ' . __('Charge Status :', 'payment-gateway-stripe-and-woocommerce-integration') . $data['captured'] . (is_null($data['transaction_id']) ? '' : '.' . __('Transaction ID : ', 'payment-gateway-stripe-and-woocommerce-integration') . $data['transaction_id']));
             WC()->cart->empty_cart();
             add_post_meta($order_id, '_sp_stripe_payment_charge', $data);
-            SP_Stripe_Log::log_update('live', $data, get_bloginfo('blogname') . ' - Charge - Order #' . $order_id);
+            SP_Stripe_Log::log_update('live', $data, get_bloginfo('blogname') . ' - ' . $session->mode == "setup" ? 'Setup Payment' : 'Charge' . ' - Order #' . $order_id);
 
             // Return thank you page redirect.
             $result =  array(
@@ -329,18 +317,6 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
                 'redirect'  => $this->get_return_url($order),
             );
 
-            /**
-             * Check order products to see if minimum fund has been raised
-             
-            foreach ($order->get_items() as $item) {
-                $product = wc_get_product($item['product_id']);
-                if ($product->get_type() == 'crowdfunding') {
-
-                    if ($this->minimum_fund_reached($product)) {
-                        $this->capture_on_hold_payments($product->ID);
-                    }
-                }
-            }*/
 
             wp_safe_redirect($result['redirect']);
             exit;
@@ -350,55 +326,6 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
         }
     }
 
-
-    /**
-     * Check on hold orders of a campaign
-     */
-    function capture_on_hold_payments($campaign_id)
-    {
-        $on_hold_orders = $this->get_orders_ids_by_product_id($campaign_id);
-        if (!empty($on_hold_orders)) {
-            foreach ($on_hold_orders as $order_id) {
-
-                $order = wc_get_order($order_id);
-                $payment_intent = get_post_meta($order_id, '_sp_payment_id', true);
-                $order_status = $order->status;
-                if (!empty($payment_intent) && $order_status == 'on-hold') {
-                    $stripe = new \Stripe\StripeClient(
-                        $this->client_id
-                    );
-                    $reponse = $stripe->paymentIntents->capture($payment_intent);
-                }
-            }
-        }
-    }
-
-    /**
-     * Get All orders IDs for a given product ID.
-     *
-     * @param  integer  $product_id (required)
-     * @param  array    $order_status (optional) Default is 'wc-completed'
-     *
-     * @return array
-     */
-    function get_orders_ids_by_product_id($product_id, $order_status = array('wc-on-hold'))
-    {
-        global $wpdb;
-
-        $results = $wpdb->get_col("
-        SELECT order_items.order_id
-        FROM {$wpdb->prefix}woocommerce_order_items as order_items
-        LEFT JOIN {$wpdb->prefix}woocommerce_order_itemmeta as order_item_meta ON order_items.order_item_id = order_item_meta.order_item_id
-        LEFT JOIN {$wpdb->posts} AS posts ON order_items.order_id = posts.ID
-        WHERE posts.post_type = 'shop_order'
-        AND posts.post_status IN ( '" . implode("','", $order_status) . "' )
-        AND order_items.order_item_type = 'line_item'
-        AND order_item_meta.meta_key = '_product_id'
-        AND order_item_meta.meta_value = '$product_id'
-    ");
-
-        return $results;
-    }
 
     /**
      * Handler when user cancel his order on stripe checkout page
@@ -444,83 +371,23 @@ class SP_Payment_Gateway extends WC_Payment_Gateway
         }
     }
 
-    /**
-     *Creates charge parameters from charge response.
-     */
-    public function make_charge_params($charge_value, $order_id)
+    function get_payment_session_checkout_url($session_id, $order)
     {
-        $wc_order = wc_get_order($order_id);
-        $charge_data = json_decode(json_encode($charge_value));
-        $origin_time = date('Y-m-d H:i:s', time() + get_option('gmt_offset') * 3600);
-        $charge_parsed = array(
-            "id" => $charge_data->id,
-            "amount" => $this->reset_stripe_amount($charge_data->amount, $charge_data->currency),
-            "amount_refunded" => $this->reset_stripe_amount($charge_data->amount_refunded, $charge_data->currency),
-            "currency" => strtoupper($charge_data->currency),
-            "order_amount" => (WC()->version < '2.7.0') ? $wc_order->order_total : $wc_order->get_total(),
-            "order_currency" => (WC()->version < '2.7.0') ? $wc_order->order_currency : $wc_order->get_currency(),
-            "captured" => $charge_data->captured ? "Captured" : "Uncaptured",
-            "transaction_id" => $charge_data->balance_transaction,
-            "mode" => (false == $charge_data->livemode) ? 'Test' : 'Live',
-            "metadata" => $charge_data->metadata,
-            "created" => date('Y-m-d H:i:s', $charge_data->created),
-            "paid" => $charge_data->paid ? 'Paid' : 'Not Paid',
-            "receiptemail" => (null == $charge_data->receipt_email) ? 'Receipt not send' : $charge_data->receipt_email,
-            "receiptnumber" => (null == $charge_data->receipt_number) ? 'No Receipt Number' : $charge_data->receipt_number,
-            "source_type" => ('card' == $charge_data->payment_method_details->type) ? $charge_data->payment_method_details->card->brand . "( " . $charge_data->payment_method_details->card->funding . " )" : 'Undefined',
-            "status" => $charge_data->status,
-            "origin_time" => $origin_time
+
+        return sprintf(
+            '#response=%s',
+            base64_encode(
+                wp_json_encode(
+                    array(
+                        'session_id' => $session_id,
+                        'order_id'      => (WC()->version < '2.7.0') ? $order->id : $order->get_id(),
+                        'time'          => rand(
+                            0,
+                            999999
+                        ),
+                    )
+                )
+            )
         );
-        $trans_time = date('Y-m-d H:i:s', time() + ((get_option('gmt_offset') * 3600) + 10));
-        $tranaction_data = array(
-            "id" => $charge_data->id,
-            "total_amount" => $charge_parsed['amount'],
-            "currency" => $charge_parsed['currency'],
-            "balance_amount" => 0,
-            "origin_time" => $trans_time
-        );
-        return $charge_parsed;
-    }
-
-    /**
-     *List of zero decimal currencies supported by stripe.
-     */
-    public static function zerocurrency()
-    {
-        return array("BIF", "CLP", "DJF", "GNF", "JPY", "KMF", "KRW", "MGA", "PYG", "RWF", "VUV", "XAF", "XOF", "XPF", "VND");
-    }
-
-    /**
-     *Gets stripe amount.
-     */
-    public  static function get_stripe_amount($total, $currency = '')
-    {
-        if (!$currency) {
-            $currency = get_woocommerce_currency();
-        }
-        if (in_array(strtoupper($currency), self::zerocurrency())) {
-            // Zero decimal currencies
-            $total = absint($total);
-        } else {
-            $total = round($total, 2) * 100; // In cents
-        }
-        return $total;
-    }
-
-    /**
-     *Reset stripe amount after charge response.
-     */
-    public function reset_stripe_amount($total, $currency = '')
-    {
-        if (!$currency) {
-            $currency = get_woocommerce_currency();
-        }
-        if (in_array(strtoupper($currency), self::zerocurrency())) {
-            // Zero decimal currencies
-            $total = absint($total);
-        } else {
-            $total = round($total, 2) / 100; // In cents
-        }
-        return $total;
     }
 }
